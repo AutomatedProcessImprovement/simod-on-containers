7352a5f383fab369deecdd7a385b6af330ed9a52
Ihar Suvorau <ihar.suvorau@gmail.com>
2023-03-21 22:37:42 +0200

    k6 added

---
+++ load-testing/k6/run.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+
+timestamp=$(date +%Y%m%d%H%M%S)
+output_dir="$REPORT_OUTPUT_DIR/$timestamp"
+mkdir -p "$output_dir"
+
+k6 run \
+--vus $K6_VUS \
+--duration $K6_DURATION \
+--rps $K6_RPS \
+--out csv=$output_dir/stats.csv \
+--no-usage-report \
+$SCRIPT_NAME
